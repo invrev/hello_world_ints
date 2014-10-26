d34929024ee28253c9192a34002779794fd026c7
vikram <abcd@gmail.com>
2014-10-26 16:45:12 -0400

    n queen solved

--- backtracking/n_queen02.cpp
+++ backtracking/n_queen02.cpp
@@ -1,4 +1,7 @@
 #include<iostream>
+#include<string>
+#include<vector>
+
 using namespace std;
 
 /* Things to consider
@@ -6,27 +9,34 @@ using namespace std;
 * 2.At max,# of queens in the col = 1
 * 3.At max,# of queen in the diagonal = 1. (upward + downward)
 */
-struct queen_pos {
-    int row;
-    int col;
-    int diagonal;
-};
 
-void print_solution (int row[],int n) {
+
+void print_solution (int row[],int n,vector< vector<string> > &solution_board) {
+   vector<string> rows(n+1);
+   string output = "";
     for (int i=0;i<n+1;i++) {
-        cout << row[i] << " " ;
+        output = "";
+        int row_no = row[i];
+        for (int j=0;j<n+1;j++) {
+            if (j != row_no ) {
+                output += ".";
+            } else {
+                output += "Q";
+            }
+        }
+        rows[i] = output ;
     }
-    cout << endl;
+    solution_board.push_back(rows);
 }
 
 
 void enum_queen(int input,int k,int c,int solution_size
-                ,bool col[],bool left_diag[],bool right_diag[],int row[],int& solution_count) {
+                ,bool col[],bool left_diag[],bool right_diag[],int row[]
+                ,vector< vector<string> > &solution_board ) {
 
       if (k == (solution_size - 1)) {
           //cout << "found the solution" <<endl;
-          print_solution (row,k);
-          solution_count++;
+          print_solution (row,k,solution_board);
       } else {
           k = k + 1;
           for (int i=0;i<c;i++) {
@@ -38,7 +48,9 @@ void enum_queen(int input,int k,int c,int solution_size
                   col[i] = false;
                   left_diag[k+i]= false;
                   right_diag[k-i+(input -1)] = false;
-                  enum_queen (input,k,c,solution_size,col,left_diag,right_diag,row,solution_count);
+                  enum_queen (input,k,c,solution_size
+                          ,col,left_diag,right_diag,row
+                          ,solution_board);
                   //RESET
                   col[i] = true;
                   left_diag[k+i]= true;
@@ -63,10 +75,22 @@ int main () {
         left_diag [i] = true;
         right_diag[i] = true;
     }
-    int solution_cnt = 0;
-
+    vector< vector<string> > solution_board;
     enum_queen(no_of_queen,-1,no_of_queen,no_of_queen
-            ,col,left_diag,right_diag,row,solution_cnt ) ;
+            ,col,left_diag,right_diag,row
+            ,solution_board) ;
 
-    cout << "solution_cnt = " << solution_cnt << endl;
+    int len = solution_board.size();
+
+    cout << "the solution size " << len << endl;
+    
+    for (int i=0;i<len;i++) {
+        int in_size = solution_board[i].size();
+        for (int j=0;j<in_size ;j++) {
+            cout << solution_board[i][j] << endl;
+        }
+        cout << endl;
+    }
+
+
 }
