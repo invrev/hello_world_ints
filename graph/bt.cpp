695952dacf508c068b50c82a19d9b6463cc9fb40
vikram <abcd@gmail.com>
2015-01-03 15:30:31 -0500

    git m

--- graph/bt.cpp
+++
@@ -1,7 +0,0 @@
-#include<iostream>
-
-//problems are of DFS type
-//eg. print the telephone numbers
-//highly DFS : iterate over digits and then over chars
-//permutation generating methods
-//eg.grapy code,subset
--- graph/dp5.cpp
+++
@@ -1,6 +0,0 @@
-#include<iostream>
-using namespace std;
-
-//Minimizing the number of multiplications by aligining the parenthesis
-
-
