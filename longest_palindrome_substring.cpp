eb7d361801787eaf5c4ca042e05fd6026f0a66f3
vikram <abcd@gmail.com>
2015-01-03 15:30:05 -0500

    git m

--- longest_palindrome_substring.cpp
+++ longest_palindrome_substring.cpp
@@ -3,14 +3,15 @@
 #include<vector>
 using namespace std;
 
+//Using Manacher algoithm
 
+//Make the string of odd length
 string pre_process (string input) {
     int len = input.length();
     string suffix = "^$";
     if (len == 0) {
        return  suffix ;
     } 
-
     string ret = "^";
     for (int i=0;i<len;i++) {
         ret += "#" + input.substr(i,1);
@@ -19,22 +20,34 @@ string pre_process (string input) {
     return ret;
 }
 
+
+//abba :  ^#a#b#b#a#$
+//aba :   ^#a#b#a#$
+
+//T = # a # b # a # a # b # a #
+//P = 0 1 0 3 0 1 6 1 0 3 0 1 0
+//Longest substring palindrome p[6] = 6
+
 string longestPalindrome (string ip) {
     string s = pre_process(ip);
     int n = s.length();
-     
+    //p : challenge is to compute the p[i]
     vector<int> p(n,0);
     int mid = 0;
     int max = 0;
 
-    //escaping the first and last char
+    //First and last character indicates the start and end  of the string so esacape it
     for (int i=1;i<n-1;i++) {
+    
+        //Choose the mirror
         int mirror = 2*mid - 1; // (mid - (i-mid) )
+        
         if (max > i) {
             p[i] = min(p[mirror],max - i);
         } else {
             p[i] = 1;
         }
+
         while (s[i +  p[i]] == s[i - p[i] ] ) {
             p[i]++;
         }
@@ -45,7 +58,7 @@ string longestPalindrome (string ip) {
             mid = i;
         }
     }
-
+    
     int max_len = 0;
     int mid_index = 0;
     
